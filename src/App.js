e97c673d04973f1d6b1b9b9c9c5b64a255882864
kimyoonjeong <gnoejnooy@gmail.com>
2023-12-30 23:38:49 +0900

    테스트 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie.js';
+import Home from './routes/Home.js';
+import Detail from './routes/Detail.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+  <Routes>
+    <Route path="/hello" element={<h1>hello</h1>} />
+    <Route path="/" element={<Home />} />
+    <Route path="/movie/:id" element={<Detail />} />
+  </Routes>
+</Router>
 }
 
 export default App;
